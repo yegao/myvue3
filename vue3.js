ba9ebffdf29d7688b2cfe4d298d0612684c0de90
yegao <familyink@163.com>
2022-11-08 21:36:39 +0800

    使用proxy代替thisthis

---
+++ vue3.js
@@ -0,0 +1,50 @@
+/**
+ * 返回App实例
+ */
+function _createApp(options) {
+    return {
+        // mount的目标：将组件配置解析为DOM，追加到宿主元素上
+        mount(selector) {
+            const host = document.querySelector(selector);
+            
+            if (!options.render) {
+                options.render = this.compile(host.innerHTML);
+            }
+            if (options.setup) {
+                this.setupState = options.setup();
+            } else {
+                this.data = options.data();
+            }
+            new Proxy(this, {
+                get(target, key) {
+                    if (key in target.setupState) {
+                        return target.setupState[key];
+                    }
+                    return target.data[key];
+                },
+                set(target, key, val) {
+                    if (key in target.setupState) {
+                        target.setupState[key] = val;
+                    } else {
+                        target.data[key] = val;
+                    }
+                }
+            })
+            const el = options.render.call(this.proxy);
+            host.innerHTML = '';
+            host.appendChild(el)
+        },
+        compile(template){
+            return function render() {
+                const div = document.createElement('div');
+                div.textContent = this.title;
+                return div;
+            }
+        }
+    }
+}
+
+
+const Vue = {
+    createApp: _createApp
+}
